96e74a1755f163ae850456fbb523b9aa824f140e
luiizsilverio <luiiz.silverio@gmail.com>
2024-03-20 19:43:22 -0300

    Redux - createAsyncThunk

--- src/views/BooksPage.jsx
+++ src/views/BooksPage.jsx
@@ -1,32 +1,22 @@
-import { useEffect, useState } from 'react';
-import { collection, query, where, getDocs } from 'firebase/firestore';
-import {useSelector} from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { db } from '../firebase/config.js';
 import Book from '../components/Book.jsx';
 import Header from '../components/Header.jsx';
 import { selectUsers } from '../store/usersSlice.js';
-// import {selectBooks} from '../store/booksSlice.js';
+import { fetchBooks, selectBooks } from '../store/booksSlice.js';
 
 function BooksPage() {
-
-  // const books = useSelector(selectBooks);
-  const [books, setBooks] = useState([]);
-  const {currentUser} = useSelector(selectUsers);    
+  const dispatch = useDispatch();
+  const { books, status: bookStatus } = useSelector(selectBooks);
+  const {currentUser} = useSelector(selectUsers);  
   
   useEffect(() => {    
-    const fetchBooks = async () => {
-      let bookList = [];
-      const q = query(collection(db, "books"), where("user_id", "==", currentUser?.id));
-      const qrySnapshot = await getDocs(q);
-      qrySnapshot.forEach((doc) => {
-        bookList.push({ id: doc.id, ...doc.data() });
-      })
-      setBooks(bookList);
+    if (bookStatus === 'idle') {
+      dispatch(fetchBooks());
     }
 
-    fetchBooks();
-  }, []);
+  }, [bookStatus, currentUser, dispatch]);
   
 
   return (
